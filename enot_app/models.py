791f70591cd4a93edfdd3e3dca89a9dac810deee
noodlesen <k.lapshov@gmail.com>
2017-06-11 15:49:04 +0300

    ...

--- enot_app/models.py
+++ enot_app/models.py
@@ -573,6 +573,7 @@ class Card(models.Model):
     link = models.CharField(max_length=255)
     tags = models.ManyToManyField(Tag)
     place = models.ForeignKey(GPlace, null=True)
+    published = models.BooleanField(default=False)
 
     def __str__(self):
         return '%s | %s - %s...' % (self.link, self.place.rus_name, self.text[:50])
