30d8d974518de9f8be580da5f20c323b14924536
noodlesen <k.lapshov@gmail.com>
2017-06-29 12:05:38 +0300

    ts2906-3

--- enot_app/urls.py
+++ enot_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.main_page, name='main_page'),
     url(r'^letter', views.letter_page, name='letter_page'),
     url(r'^build/(?P<dts>\w+)', views.build, name='build'),
+    url(r'^loadtrips/(?P<dts>\w+)', views.load_trips, name='load_trips'),
     url(r'^outdated', views.outdated, name='outdated'),
     url(r'^unsubscribe/(?P<hsh>\w+)', views.unsubscribe, name='unsubscribe'),
     url(r'^(?P<no>\d+)$', views.ticket_no, name='ticketno'),
